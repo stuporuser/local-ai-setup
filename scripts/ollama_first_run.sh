da1a3c8d98305d0fc5a7bd03e35165ffa622b0de
stuporuser <geoffrey.shraga@gmail.com>
2025-06-04 16:34:50 -0400

    Syntax change for parsing models for Ollama to pull in first-run script.

--- scripts/ollama_first_run.sh
+++ scripts/ollama_first_run.sh
@@ -5,7 +5,7 @@ if test -f "/root/.ollama/.has_been_run"; then
 fi
 
 #IFS=',' read -r -a models_array <<< "$MODELS_TO_PULL"
-models_array=($(echo "$MODELS_TO_PULL" | tr ',' ' '))
+models_array=($(echo $MODELS_TO_PULL | tr ',' ' '))
 
 for model in "${models_array[@]}"; do
     ollama pull "$model"
