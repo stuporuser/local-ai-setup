a9f01297e99d15f2c39384d4a89628bb965d4566
stuporuser <geoffrey.shraga@gmail.com>
2025-06-04 16:28:35 -0400

    Add n8n-import service as first-run.

--- scripts/ollama_first_run.sh
+++ scripts/ollama_first_run.sh
@@ -4,7 +4,8 @@ if test -f "/root/.ollama/.has_been_run"; then
     exit 0
 fi
 
-IFS=',' read -r -a models_array <<< "$MODELS_TO_PULL"
+#IFS=',' read -r -a models_array <<< "$MODELS_TO_PULL"
+models_array=($(echo "$MODELS_TO_PULL" | tr ',' ' '))
 
 for model in "${models_array[@]}"; do
     ollama pull "$model"
